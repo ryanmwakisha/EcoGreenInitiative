fcea1bc2d022a9a248d24c984415961ebe18a8e9
ryan <ryakishn@gmail.com>
2025-08-01 00:36:25 +0300

    logout

--- logout.php
+++ logout.php
@@ -2,16 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
+header("Location: login.php?loggedout=true");
+exit();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Logged Out</title>
-    <meta http-equiv="refresh" content="3;url=login.php">
-</head>
-<body>
-    <h2>You have been successfully logged out.</h2>
-    <p>Redirecting to login page in a few seconds...</p>
-</body>
-</html>
-
